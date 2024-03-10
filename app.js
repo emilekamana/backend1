b820a491ca5fe88a567dde326c0afa287491fd35
nelsa01 <n.ishimwe@alustudent.com>
2024-03-10 13:18:10 +0200

    mmm

--- app.js
+++ app.js
@@ -61,9 +61,9 @@ __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
   // app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  // app.get('*', (req, res) =>
-  //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  // )
+  app.get('*', (req, res) =>
+  res.send('API is running....')
+  )
   console.log("done")
 
 } else {
